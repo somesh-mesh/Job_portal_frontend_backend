9afb17b5c794a3bfe3a203005714c50181b15aa5
Hoora <meshramsomesh00@gmail.com>
2024-10-08 01:37:01 +0530

    added loader in login button

--- Frontend/src/components/auth/Login.jsx
+++ Frontend/src/components/auth/Login.jsx
@@ -8,6 +8,10 @@ import { Link, useNavigate } from "react-router-dom";
 import { toast } from "sonner";
 import axios from "axios";
 import { USER_API_END_POINT } from "@/utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import store from "@/redux/store";
+import { Loader2 } from "lucide-react";
 
 const Login = () => {
   const [input, setInput] = useState({
@@ -17,7 +21,9 @@ const Login = () => {
     file: "",
   });
 
+  const { loading } = useSelector(store => store.auth);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const changeEventHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
@@ -28,6 +34,7 @@ const Login = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
+      dispatch(setLoading(true));
       const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
         headers: {
           "Content-Type": "application/json",
@@ -42,9 +49,11 @@ const Login = () => {
       console.error("Login Error:", error.response?.data); // Log detailed error from server
       const errorMessage = error.response?.data?.message || "An error occurred. Please try again.";
       toast.error(errorMessage);
+    } finally {
+      dispatch(setLoading(false));
     }
   };
-  
+
 
   return (
     <div>
@@ -104,9 +113,12 @@ const Login = () => {
               </div>
             </RadioGroup>
           </div>
-          <Button type="submit" className="w-full my-4">
-            Login
-          </Button>
+          {
+            loading ? <Button className="w-full my-4"><Loader2 className="mr-2 h-4 w-4 animate-spin" />Please wait</Button> : <Button type="submit" className="w-full my-4">
+              Login
+            </Button>
+          }
+
           <span>
             Don't have an account?{" "}
             <Link to="/signup" className="text-blue-600">
